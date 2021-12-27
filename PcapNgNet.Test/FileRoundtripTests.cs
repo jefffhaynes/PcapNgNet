969ca37efbd8019ec67a096380c5a2b9cd6ed0b2
Greg Ingram <shaggygi97@gmail.com>
2021-12-27 16:17:20 -0500

    more namespace file scope

--- PcapNgNet.Test/FileRoundtripTests.cs
+++ PcapNgNet.Test/FileRoundtripTests.cs
@@ -5,154 +5,153 @@ using System.Linq;
 using BinarySerialization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace PcapNgNet.Test
+namespace PcapNgNet.Test;
+
+[TestClass]
+public class FileRoundtripTests
 {
-    [TestClass]
-    public class FileRoundtripTests
+    [TestMethod]
+    public void RoundtripBasicLeFiles()
     {
-        [TestMethod]
-        public void RoundtripBasicLeFiles()
+        RoundtripFiles("Files//LE//Basic");
+    }
+
+    [TestMethod]
+    public void RoundtripAdvancedLeFiles()
+    {
+        RoundtripFiles("Files//LE//Advanced", true);
+    }
+
+    [TestMethod]
+    public void RoundtripDifficultLeFiles()
+    {
+        RoundtripFiles("Files//LE//Difficult", true);
+    }
+
+    [TestMethod]
+    public void RoundtripBasicBeFiles()
+    {
+        RoundtripFiles("Files//BE//Basic");
+    }
+
+    [TestMethod]
+    public void RoundtripAdvancedBeFiles()
+    {
+        RoundtripFiles("Files//BE//Advanced", true);
+    }
+
+    private void RoundtripFiles(string path, bool ignoreSerializationErrors = false)
+    {
+        var serializer = new PcapNgSerializer();
+
+        serializer.Serializer.MemberSerializing += OnMemberSerializing;
+        serializer.Serializer.MemberSerialized += OnMemberSerialized;
+        serializer.Serializer.MemberDeserializing += OnMemberDeserializing;
+        serializer.Serializer.MemberDeserialized += OnMemberDeserialized;
+
+        var files = Directory.EnumerateFiles(path);
+
+        foreach (var file in files)
         {
-            RoundtripFiles("Files//LE//Basic");
-        }
+            Debug.WriteLine($"Roundtrip for {file}");
 
-        [TestMethod]
-        public void RoundtripAdvancedLeFiles()
-        {
-            RoundtripFiles("Files//LE//Advanced", true);
-        }
-
-        [TestMethod]
-        public void RoundtripDifficultLeFiles()
-        {
-            RoundtripFiles("Files//LE//Difficult", true);
-        }
-
-        [TestMethod]
-        public void RoundtripBasicBeFiles()
-        {
-            RoundtripFiles("Files//BE//Basic");
-        }
-
-        [TestMethod]
-        public void RoundtripAdvancedBeFiles()
-        {
-            RoundtripFiles("Files//BE//Advanced", true);
-        }
-
-        private void RoundtripFiles(string path, bool ignoreSerializationErrors = false)
-        {
-            var serializer = new PcapNgSerializer();
-
-            serializer.Serializer.MemberSerializing += OnMemberSerializing;
-            serializer.Serializer.MemberSerialized += OnMemberSerialized;
-            serializer.Serializer.MemberDeserializing += OnMemberDeserializing;
-            serializer.Serializer.MemberDeserialized += OnMemberDeserialized;
-
-            var files = Directory.EnumerateFiles(path);
-
-            foreach (var file in files)
+            using (var stream = new FileStream(file, FileMode.Open, FileAccess.Read))
             {
-                Debug.WriteLine($"Roundtrip for {file}");
+                var pcap = serializer.Deserialize(stream);
 
-                using (var stream = new FileStream(file, FileMode.Open, FileAccess.Read))
+                var unknownSection =
+                    pcap.Sections.SelectMany(section => section.Blocks)
+                        .Any(block => block.Body.GetType() == typeof(UnknownBlockBody));
+
+                if (unknownSection)
+                    continue;
+
+                try
                 {
-                    var pcap = serializer.Deserialize(stream);
+                    var memStream = new MemoryStream();
+                    serializer.Serialize(memStream, pcap);
 
-                    var unknownSection =
-                        pcap.Sections.SelectMany(section => section.Blocks)
-                            .Any(block => block.Body.GetType() == typeof (UnknownBlockBody));
+                    stream.Position = 0;
+                    memStream.Position = 0;
 
-                    if(unknownSection)
-                        continue;
-
-                    try
-                    {
-                        var memStream = new MemoryStream();
-                        serializer.Serialize(memStream, pcap);
-
-                        stream.Position = 0;
-                        memStream.Position = 0;
-
-                        AssertAreEqual(stream, memStream);
-                    }
-                    catch
-                    {
-                        if (!ignoreSerializationErrors)
-                            throw;
-                    }
+                    AssertAreEqual(stream, memStream);
+                }
+                catch
+                {
+                    if (!ignoreSerializationErrors)
+                        throw;
                 }
             }
         }
+    }
 
-        private static void AssertAreEqual(Stream expected, Stream actual)
+    private static void AssertAreEqual(Stream expected, Stream actual)
+    {
+        const int blockSize = 4096;
+        var expectedBlock = new byte[blockSize];
+        var actualBlock = new byte[blockSize];
+
+        long offset = 0;
+        while (true)
         {
-            const int blockSize = 4096;
-            var expectedBlock = new byte[blockSize];
-            var actualBlock = new byte[blockSize];
+            int expectedCount = expected.Read(expectedBlock, 0, blockSize);
+            int actualCount = actual.Read(actualBlock, 0, blockSize);
 
-            long offset = 0;
-            while (true)
+            if (expectedCount != actualCount)
             {
-                int expectedCount = expected.Read(expectedBlock, 0, blockSize);
-                int actualCount = actual.Read(actualBlock, 0, blockSize);
-
-                if (expectedCount != actualCount)
-                {
-                    Assert.Fail($"Expected count was {expectedCount} but actual count was {actualCount}.");
-                }
-
-                if (expectedCount == 0)
-                    return;
-
-                for (int i = 0; i < expectedCount; i++)
-                {
-                    if (expectedBlock[i] != actualBlock[i])
-                    {
-                        Assert.Fail($"Mismatch at position {offset}: expected {expectedBlock[i]} but was {actualBlock[i]}.");
-                    }
-
-                    offset++;
-                }
-            }
-        }
-
-        private static void PrintIndent(int depth)
-        {
-            var indent = new string(Enumerable.Repeat(' ', depth * 4).ToArray());
-            Debug.Write(indent);
-        }
-
-        private static void OnMemberSerializing(object sender, MemberSerializingEventArgs e)
-        {
-            PrintIndent(e.Context.Depth);
-            Debug.WriteLine("S-Start: {0} @ {1}", e.MemberName, e.Offset);
-        }
-
-        private static void OnMemberSerialized(object sender, MemberSerializedEventArgs e)
-        {
-            PrintIndent(e.Context.Depth);
-            var value = e.Value ?? "null";
-            Debug.WriteLine("S-End: {0} ({1}) @ {2}", e.MemberName, value, e.Offset);
-        }
-
-        private static void OnMemberDeserializing(object sender, MemberSerializingEventArgs e)
-        {
-            PrintIndent(e.Context.Depth);
-            Debug.WriteLine("D-Start: {0} @ {1}", e.MemberName, e.Offset);
-        }
-
-        private static void OnMemberDeserialized(object sender, MemberSerializedEventArgs e)
-        {
-            PrintIndent(e.Context.Depth);
-            var value = e.Value ?? "null";
-
-            if (value is byte[])
-            {
-                value = BitConverter.ToString((byte[])value);
+                Assert.Fail($"Expected count was {expectedCount} but actual count was {actualCount}.");
             }
 
-            Debug.WriteLine("D-End: {0} ({1}) @ {2}", e.MemberName, value, e.Offset);
+            if (expectedCount == 0)
+                return;
+
+            for (int i = 0; i < expectedCount; i++)
+            {
+                if (expectedBlock[i] != actualBlock[i])
+                {
+                    Assert.Fail($"Mismatch at position {offset}: expected {expectedBlock[i]} but was {actualBlock[i]}.");
+                }
+
+                offset++;
+            }
         }
+    }
+
+    private static void PrintIndent(int depth)
+    {
+        var indent = new string(Enumerable.Repeat(' ', depth * 4).ToArray());
+        Debug.Write(indent);
+    }
+
+    private static void OnMemberSerializing(object sender, MemberSerializingEventArgs e)
+    {
+        PrintIndent(e.Context.Depth);
+        Debug.WriteLine("S-Start: {0} @ {1}", e.MemberName, e.Offset);
+    }
+
+    private static void OnMemberSerialized(object sender, MemberSerializedEventArgs e)
+    {
+        PrintIndent(e.Context.Depth);
+        var value = e.Value ?? "null";
+        Debug.WriteLine("S-End: {0} ({1}) @ {2}", e.MemberName, value, e.Offset);
+    }
+
+    private static void OnMemberDeserializing(object sender, MemberSerializingEventArgs e)
+    {
+        PrintIndent(e.Context.Depth);
+        Debug.WriteLine("D-Start: {0} @ {1}", e.MemberName, e.Offset);
+    }
+
+    private static void OnMemberDeserialized(object sender, MemberSerializedEventArgs e)
+    {
+        PrintIndent(e.Context.Depth);
+        var value = e.Value ?? "null";
+
+        if (value is byte[])
+        {
+            value = BitConverter.ToString((byte[])value);
+        }
+
+        Debug.WriteLine("D-End: {0} ({1}) @ {2}", e.MemberName, value, e.Offset);
     }
 }
--- PcapNgNet.Tester/Program.cs
+++ PcapNgNet.Tester/Program.cs
@@ -2,49 +2,48 @@
 using System.IO;
 using BinarySerialization;
 
-namespace PcapNgNet.Tester
+namespace PcapNgNet.Tester;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var serializer = new BinarySerializer();
+
+        serializer.MemberSerializing += OnMemberSerializing;
+        serializer.MemberSerialized += OnMemberSerialized;
+        serializer.MemberDeserializing += OnMemberDeserializing;
+        serializer.MemberDeserialized += OnMemberDeserialized;
+
+        using (var stream = new FileStream(args[0], FileMode.Open, FileAccess.Read))
         {
-            var serializer = new BinarySerializer();
-
-            serializer.MemberSerializing += OnMemberSerializing;
-            serializer.MemberSerialized += OnMemberSerialized;
-            serializer.MemberDeserializing += OnMemberDeserializing;
-            serializer.MemberDeserialized += OnMemberDeserialized;
-
-            using (var stream = new FileStream(args[0], FileMode.Open, FileAccess.Read))
-            {
-                var pcap = serializer.Deserialize<PcapNg>(stream);
-            }
+            var pcap = serializer.Deserialize<PcapNg>(stream);
         }
+    }
 
-        private static void OnMemberSerializing(object sender, MemberSerializingEventArgs e)
-        {
-            Console.CursorLeft = e.Context.Depth * 4;
-            Console.WriteLine("S-Start: {0} @ {1}", e.MemberName, e.Offset);
-        }
+    private static void OnMemberSerializing(object sender, MemberSerializingEventArgs e)
+    {
+        Console.CursorLeft = e.Context.Depth * 4;
+        Console.WriteLine("S-Start: {0} @ {1}", e.MemberName, e.Offset);
+    }
 
-        private static void OnMemberSerialized(object sender, MemberSerializedEventArgs e)
-        {
-            Console.CursorLeft = e.Context.Depth * 4;
-            var value = e.Value ?? "null";
-            Console.WriteLine("S-End: {0} ({1}) @ {2}", e.MemberName, value, e.Offset);
-        }
+    private static void OnMemberSerialized(object sender, MemberSerializedEventArgs e)
+    {
+        Console.CursorLeft = e.Context.Depth * 4;
+        var value = e.Value ?? "null";
+        Console.WriteLine("S-End: {0} ({1}) @ {2}", e.MemberName, value, e.Offset);
+    }
 
-        private static void OnMemberDeserializing(object sender, MemberSerializingEventArgs e)
-        {
-            Console.CursorLeft = e.Context.Depth * 4;
-            Console.WriteLine("D-Start: {0} @ {1}", e.MemberName, e.Offset);
-        }
+    private static void OnMemberDeserializing(object sender, MemberSerializingEventArgs e)
+    {
+        Console.CursorLeft = e.Context.Depth * 4;
+        Console.WriteLine("D-Start: {0} @ {1}", e.MemberName, e.Offset);
+    }
 
-        private static void OnMemberDeserialized(object sender, MemberSerializedEventArgs e)
-        {
-            Console.CursorLeft = e.Context.Depth * 4;
-            var value = e.Value ?? "null";
-            Console.WriteLine("D-End: {0} ({1}) @ {2}", e.MemberName, value, e.Offset);
-        }
+    private static void OnMemberDeserialized(object sender, MemberSerializedEventArgs e)
+    {
+        Console.CursorLeft = e.Context.Depth * 4;
+        var value = e.Value ?? "null";
+        Console.WriteLine("D-End: {0} ({1}) @ {2}", e.MemberName, value, e.Offset);
     }
 }
